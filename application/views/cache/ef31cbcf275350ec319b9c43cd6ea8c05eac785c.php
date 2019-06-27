1a7b2ccc7e5e1e86b052b0efc438937a7e536619
Bertho <bertho@pawoon.com>
2019-06-28 01:27:53 +0700

    fixed untracked files

--- application/views/cache/ef31cbcf275350ec319b9c43cd6ea8c05eac785c.php
+++
@@ -1,9 +0,0 @@
-<?php $__currentLoopData = $users; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $user): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-	<?php echo e($user->id); ?> <br>
-	<?php echo e($user->nama); ?> <br>
-	<?php echo e($user->username); ?> <br>
-	<?php echo e($user->email); ?>
-
-	<hr>
-<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-<?php /**PATH D:\laragon\www\nominal\application\views/api.blade.php ENDPATH**/ ?>
