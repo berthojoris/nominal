e09e96511ae19d8ed176814d4e8a1547055c9036
Bertho <bertho@pawoon.com>
2019-07-25 12:13:04 +0700

    fixed untracked files

--- scheduler/dumps.sh
+++
@@ -1,3 +0,0 @@
-mysqldump -h 131.100.55.58 -P 1000 -u root -pP@ssw0rd cacti host > /var/www/html/nominal/scheduler/dump_host.sql
-mysql -h 172.18.65.56 -u raga -ppassword wanvolution_dev_backup < /var/www/html/nominal/scheduler/dump_host.sql
-mysql -h 172.18.65.56 -u raga -ppassword wanvolution_dev_backup < /var/www/html/nominal/scheduler/update_db.sql
