2f23caac5fad841cd8be1988a074d3bdb287254f
Bertho <berthojoris@gmail.com>
2019-10-21 10:50:25 +0700

    Add order by

--- application/models/M_relokasi.php
+++ application/models/M_relokasi.php
@@ -2,11 +2,11 @@
 
 class M_relokasi extends CI_Model {
 
-    function data($number, $offset){
-		return $this->db->get('tb_relokasi', $number, $offset)->result();		
-    }
-    
-    function jumlah_data(){
+	function data($number, $offset) {
+		return $this->db->order_by("id", "desc")->get('tb_relokasi', $number, $offset)->result();		
+	}
+
+	function jumlah_data() {
 		return $this->db->get('tb_relokasi')->num_rows();
 	}
 }
