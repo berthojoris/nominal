87701678a6239215c82787c388c2ab31c2636cab
Bertho <berthojoris@gmail.com>
2019-10-21 10:34:46 +0700

    Default $config['index_page']

--- application/config/config.php
+++ application/config/config.php
@@ -36,7 +36,7 @@ $config['base_url'] = 'http://nominal.con';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
