f840382cc6fc3ee0c64867833c627e1e99436d0e
Bertho <berthojoris@gmail.com>
2019-11-21 14:34:35 +0700

    Update notif

--- application/views/adm_operation/modal/alert.php
+++ application/views/adm_operation/modal/alert.php
@@ -1,21 +1,8 @@
-<div class="modal fade" id="notificationModal">
-    <div class="modal-dialog" style="width: 500px;">
-        <div class="modal-content" >
-            <div class="modal-header" style="background-color:#3C8DBC;color:#FFFFFF;font-weight:bold;font-size:14pt;">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Notification</h4>
-            </div>
-            <div class="modal-body"><?php echo $this->session->flashdata('notifMessage'); ?></div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-</div>
- 
+<?php if($this->session->flashdata('notifMessage')) { ?>
+    <input type="hidden" id="notif" value="<?php echo $this->session->flashdata('notifMessage') ?>">
+<?php } ?>
 <script type="text/javascript">
     <?php if($this->session->flashdata('notifMessage')) { ?>
-        $("#notificationModal").modal('show');
+        swal("Notification", $("#notif").val(), "success");
     <?php } ?>
 </script>
