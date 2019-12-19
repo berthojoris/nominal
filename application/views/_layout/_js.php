94525e6d8d25508b6efa5f50924b5579f0475a5f
Bertho <berthojoris@gmail.com>
2019-12-19 10:15:15 +0700

    Remove unused import js

--- application/views/_layout/_js.php
+++ application/views/_layout/_js.php
@@ -1,8 +1,6 @@
 <!-- REQUIRED JS SCRIPTS -->
 <!-- Bootstrap 3.3.6 -->
 <script type="text/javascript" src="<?=base_url()?>assets/js/jquery.datetimepicker.full.min.js"></script>
-<!--<script src="<?php echo base_url(); ?>assets/js/ajax.js"></script>-->
-<!-- <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script> -->
 <script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/plugins/select2/select2.full.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/plugins/iCheck/icheck.min.js"></script>
@@ -10,24 +8,9 @@
 <script src="<?php echo base_url(); ?>assets/plugins/datatables/dataTables.bootstrap.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo base_url(); ?>assets/dist/js/app.min.js"></script>
-<!-- <script src="<?php //echo base_url(); ?>assets/js/jquery.min.js"></script>-->
-<!--<script src="<?php echo base_url(); ?>assets/eksternal/fontawesome-563/js/all.js"></script>-->
-
-
-<!--<script src="<?php echo base_url(); ?>assets/SweetAlert.js"></script>-->
-
-
 <script src="<?php echo base_url(); ?>assets/plugins/sumoselect/jquery.sumoselect.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/loader/loading.js"></script>
-<!--<script src="<?php echo base_url(); ?>assets/plugins/multiselect/js/bootstrap-multiselect.js"></script>-->
 
 <!-- My Ajax -->
 <?php 
-	$path = "localhost/portal_noc_ojk/assets/js/ajax.js";
-
-	//echo "Path : $path";
-
-	//require "$path";
-
-	//include "$path"; 
+$path = "localhost/portal_noc_ojk/assets/js/ajax.js";
 ?>
